91893e267cbd2702adeee2b16027c044aadf6c99
Bruno-Trojahn-Bolzan <107437327+Bruno-Trojahn-Bolzan@users.noreply.github.com>
2022-06-28 04:57:57 -0300

    Fixed all client info query

--- Api/project/src/main/java/br/com/unisc/project/repositories/ClienteAllInfoRepository.java
+++ Api/project/src/main/java/br/com/unisc/project/repositories/ClienteAllInfoRepository.java
@@ -11,7 +11,7 @@ import br.com.unisc.project.entities.ClientAllInfoEntity;
 @Repository
 public interface ClienteAllInfoRepository extends JpaRepository<ClientAllInfoEntity, Long>{
 
-	@Query(value = "select id, cpf_cnpj, name, phone, (select count(id) from history h where h.cliente_id = c.id) as num_queries, (select avg(p.price) from product p where p.id in (select product_id from history h where h.cliente_id = c.id)) as price_mean from client c", nativeQuery = true)
+	@Query(value = "select distinct c.id, cpf_cnpj, name, phone, (select count(id) from history h where h.cliente_id = c.id) as num_queries, (select avg(p.price) from client cl join history h on c.id = h.cliente_id join product p on h.product_id = p.id) as price_mean from client c, history hi where hi.cliente_id = c.id", nativeQuery = true)
 	List<ClientAllInfoEntity> findClientAllDto();
 
 }
